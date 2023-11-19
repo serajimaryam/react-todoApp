fa14ecf3ab2bc83a5eb6475b7b44e5e9a9f84da1
mary <seraji.maryam1982@gmail.com>
2023-11-19 12:19:55 +0330

    some files deleted

--- src/components/TodoInput.js
+++
@@ -1,9 +0,0 @@
-export default function TodoInput(){
-    return(
-        <form id="todo-form">
-        <input type="text" placeholder="Write your todo.."  id="title"/>
-        <button type="submit" id="save-btn">Save</button>
-        </form>
-        
-    );
-}
